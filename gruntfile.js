559d9b34ef4d1cf5b23d672460c843d7e1235565
zhang060606 <1320642432@qq.com>
2018-11-13 17:27:18 +0800

    全部重新提交

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+//gruntfile.js
+//模块化导入函数
+module.exports = function(grunt){
+	//所有插件的配置信息
+	grunt.initConfig({
+		//获取package.json
+		pkg:grunt.file.readJSON("package.json"),
+		//gulif插件的配件信息
+		uglify:{
+			options:{
+				banner:'/*!<%= pkg.name %> <%= pkg.version %> 发布日期:<%= grunt.template.today("yyyy-mm-dd")%>*/',
+			},
+			build:{
+				src:"src/js/wipe.js",
+				dest:"build/js/wipe-<%= pkg.version %>.min.js"
+			}
+		},
+		cssmin:{
+			options:{
+				mergeIntoShorthands:false,
+				roundingPrecision:-1
+			},
+			target:{
+				files:[{
+					expand:true,
+					cwd:'src/css',
+					src:['*.css'],
+					dest:'build/css',
+					ext:'.min.css'
+				}]
+			}
+		},
+		clean:{
+			dest:['build/*','sample/js/*']
+		},
+		jshint:{
+			test:['src/js/wipe.js'],
+			options:{
+				jshintrc:'.jshintrc'
+			}
+		},
+		copy: {
+		    js: {expand: true, cwd: 'src/js/', src: '*.min.js', dest: 'sample/js/'}
+		}
+	});
+	//告诉grunt需要使用插件
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	//告诉grunt当我们输入grunt命令后需要这些什么，有先后顺序
+	grunt.registerTask('default',['jshint','clean','uglify','copy']);
+
+};
---
+++ src/js/wipe.js
@@ -0,0 +1,136 @@
+var cas = document.getElementById('cas');
+var context = cas.getContext("2d");
+var _w = cas.width;
+var _h = cas.height;
+var radius = 20;//涂抹的半径
+var isMouseDown = false;//表示鼠标的状态,是否按下,默认未按下false,按下true
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+// 判断是手机端还是PC端
+var press = device ? "touchstart" : "mousedown";
+var move = device ? "touchmove" : "mousemove";
+var loosen = device ? "touchend" : "mouseup";
+function drawRect(context){
+	context.save();
+	context.beginPath();
+	context.fillStyle = "#666";
+	context.fillRect(0,0,_w,_h);
+	context.globalCompositeOperation = "destination-out";
+}
+function drawT(context,x1,y1,x2,y2){
+	if (arguments.length === 3) {
+		//画点功能
+		context.save();
+		context.beginPath();
+		context.arc(x1,y1,radius,0,2*Math.PI);
+		context.fillStyle = "red";
+		context.fill();
+		context.restore();
+	}else{
+		//画线功能
+		context.save();
+		context.lineCap = "round";
+		context.beginPath();
+		context.moveTo(x1,y1);
+		context.lineTo(x2,y2);
+		context.lineWidth = radius*2;
+		context.stroke();
+		context.restore();
+	}
+
+}
+// 在canvas画布上监听自定义事件"mousedown",调用drawPoint函数
+cas.addEventListener(press,function(evt){
+	var event = evt || window.event;
+	//获取鼠标在视口的坐标,传递参数到drawPoint
+	x1 = device ? event.touches[0].clientX : event.clientX;
+	y1 = device ? event.touches[0].clientY : event.clientY;
+	isMouseDown = true;
+	drawT(context,x1,y1);
+},false);
+cas.addEventListener(move,function(evt){
+	if (!isMouseDown ) {
+		return false;
+	}else{
+		var event = evt || window.event;
+		event.preventDefault();
+		event.preventDefault();
+		x2 = device ? event.touches[0].clientX : event.clientX;
+		y2 = device ? event.touches[0].clientY : event.clientY;
+		//drawPoint(context,a,b);
+		drawT(context,x1,y1,x2,y2);
+		//每次的结束点变成下一次划线的开始点
+		x1 = x2;
+		y1 = y2;
+	}
+},false);
+// 手势触摸
+// cas.addEventListener("touchstart",function(evt){
+// 	isMouseDown=true;
+// 	var event = evt || window.event;
+// 	//获取鼠标在视口的坐标,传递参数到drawPoint
+// 	moveX = event.touches[0].clientX;
+// 	moveY = event.touches[0].clientY;
+// 	drawarv(context,moveX,moveY);
+// },false);
+// 手势滑动
+// cas.addEventListener("touchmove",fn1,false);
+// function fn1(evt){
+// 	//判断,当isMouseDown为true时,才执行下面的操作
+// 	if (!isMouseDown ) {
+// 		return false;
+// 	}else{
+// 		var event = evt || window.event;
+// 		event.preventDefault();
+// 		var x2 = event.touches[0].clientX;
+// 		var y2 = event.touches[0].clientY;
+// 		//drawPoint(context,a,b);
+// 		drawT(context,x1,y1,x2,y2);
+// 		//每次的结束点变成下一次划线的开始点
+// 		x1 = x2;
+// 		y1 = y2;
+// 	}
+// }
+cas.addEventListener(loosen,function(){
+	var c = 0;
+	var a = context.getImageData(0,0,_w,_h);
+		console.log(a);
+	for (var j = 0; j < _h; j++) {
+		for (var i = 0; i < _w; i++) {
+			var f = ((_w*j)+i)*4+3;
+			if (a.data[f] == 0) {	
+				c++;
+			}
+		}
+	}
+	var tmb = c/(_w*_h)*100;
+	console.log(Math.round(tmb));
+	if (tmb > 60) {
+		clearRect(context);
+	}
+	isMouseDown = false;
+},false);
+// cas.addEventListener("touchend",function(){
+// 	var c = 0;
+// 	var a = context.getImageData(0,0,_w,_h);
+// 		console.log(a);
+// 	for (var j = 0; j < _h; j++) {
+// 		for (var i = 0; i < _w; i++) {
+// 			var f = ((_w*j)+i)*4+3;
+// 			if (a.data[f] == 0) {	
+// 				c++;
+// 			}
+// 		}
+// 	}
+// 	var tmb = c/(_w*_h)*100;
+// 	console.log(Math.round(tmb));
+// 	if (tmb > 60) {
+// 		clearRect(context);
+// 	}
+// 	isMouseDown = false;
+// },false);
+function clearRect(context){
+	context.clearRect(0,0,_w,_h);
+}
+window.onload = function(){
+	drawRect(context);
+};
